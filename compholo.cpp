3d5baf37d0d92c4972e470e9d13b7d284cfc020b
CEBasile <cbasile2@gmail.com>
2016-12-01 14:56:45 -0500

    Add files via upload
    Updated for correct types

--- compholo.cpp
+++ compholo.cpp
@@ -11,7 +11,8 @@ int main()
 	double wavelength = 0.000000630;
 	double sampling = 0.0254/dpi;
 	double n, dx, dy, dz, dist; 
-	complex<doubl> cw. film;
+	complex<double> cw;
+	vector< complex<double> > film;
 	int i, j, k, dimensions = 1;
 	double range = dimensions*.0254/2;
 	vector<double> ipx, ipy, film, objpts;
@@ -30,10 +31,20 @@ int main()
 				dy = objpts[i][1] - ipy[k];
 				dz = objpts[i][2] - 0; //Place holder for 3D compositions
 				dist = sqrt(dx*dx+dy*dy+dz*dz);
-				cw = exp(2*M_PI*sqrt(-1)*dist/wavelength);
+				cw = exp(2*M_PI*sqrt(-1)*dist/wavelength); //Not correct format for complex
 				film[i][j] += cw;
 			}
 		}
 	}
 //Extract real part of film with film.real() and write to an images useing lodepng
+int width = dpi, height = dpi;
+vector<int char> image = {};
+image.resize(width * height * 4);
+for(int y = 0; y < height; y++)
+for(int x = 0; x < width; x++) {
+	image[4 * width * y + 4 * x + 0] = 255;
+    image[4 * width * y + 4 * x + 1] = 255;
+    image[4 * width * y + 4 * x + 2] = 255;
+    image[4 * width * y + 4 * x + 3] = 255;
+	}
 }
